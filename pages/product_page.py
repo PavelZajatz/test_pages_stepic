92f9f29f4e824787be529ba8e633b84fa1bf5d93
Pavluha44 <52718431+Pavluha44@users.noreply.github.com>
2020-04-21 23:52:55 +0300

    Добавлены отрицательные проверки

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,25 +1,31 @@
 from .base_page import BasePage
-from .locators import LoginPageLocators
-from .locators import BasketPageLocators
-from selenium.webdriver.common.by import By
+from .locators import ProductPageLocators
 
 
-class BasketPage(BasePage):
+class ProductPage(BasePage):
 
-    def should_add_item_to_basket(self):
-        assert self.is_element_present(*BasketPageLocators.ADD_TO_BASKET), "Login url is not presented"
-        add_btn = self.browser.find_element(*BasketPageLocators.ADD_TO_BASKET)
+    def add_to_basket(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET), "Button is not presented"
+        add_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
         add_btn.click()
 
     def should_be_book_price(self):
-        book_price = self.browser.find_element(*BasketPageLocators.BOOK_PRICE).text
-        basket_price = self.browser.find_element(*BasketPageLocators.BOOK_PRICE_BASKET).text
-        assert  book_price == basket_price, "Price is not same"
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
+        basket_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE_BASKET).text
+        assert book_price == basket_price, "Price is not same"
 
     def should_be_book_name(self):
-        book_name = self.browser.find_element(*BasketPageLocators.BOOK_NAME).text
-        book_basket = self.browser.find_element(*BasketPageLocators.BOOK_NAME_BASKET).text
+        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+        book_basket = self.browser.find_element(*ProductPageLocators.BOOK_NAME_BASKET).text
         assert book_name == book_basket, "Name is not same"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented"
+
+    def should_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
 
 
 
--- test_product_page.py
+++ test_product_page.py
@@ -1,27 +1,49 @@
-from .pages.main_page import MainPage
-from .pages.product_page import BasketPage
+from .pages.product_page import ProductPage
 import time
 import pytest
-
+"""
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+#                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = BasketPage(browser, link)  #инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()
-    page.should_add_item_to_basket() #добавляем товар в корзину
+"""
+@pytest.mark.xfail(reason="should not be present")
+def test_guest_can_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_to_basket()
+    #product_page.solve_quiz_and_get_code()
+    #time.sleep(5)
+    product_page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_not_be_success_message()
+
+
+@pytest.mark.xfail(reason="not disappiring")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_to_basket()
     time.sleep(1)
-    page.solve_quiz_and_get_code() #решаем quiz в алерте
-    time.sleep(1)
-    page.should_be_book_name() #проверяем имя книги с именем добавленной в корзину книги
-    time.sleep(1)
-    page.should_be_book_price() #проверяем цену книги на странице и цену книги в корзине
-    time.sleep(1)
+    product_page.should_disappeared()
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.should_be_book_name()
+    product_page.should_be_book_price()
