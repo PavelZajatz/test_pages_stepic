6cdf8e316522a49aeee8ea5264c924652b7282bd
2761421@jabil.com <52718431+PavelZajatz@users.noreply.github.com>
2020-11-04 18:37:24 +0200

    compose

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 
 
@@ -24,11 +25,13 @@ def browser(request):
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_experimental_option("prefs", {"intl.accept_languages": user_language})
-        browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        # browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        browser = webdriver.Remote('http://10.6.0.30:4444/wd/hub', desired_capabilities=DesiredCapabilities.CHROME)
     elif browser_name == "firefox":
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
-        browser = webdriver.Firefox(executable_path=GeckoDriverManager().install(), firefox_profile=fp)
+        #browser = webdriver.Firefox(executable_path=GeckoDriverManager().install(), firefox_profile=fp)
+        browser = webdriver.Remote('http://10.6.0.30:4444/wd/hub', desired_capabilities=DesiredCapabilities.FIREFOX)
     else:
         print("Browser <browser_name> is still not implemented")
     yield browser
